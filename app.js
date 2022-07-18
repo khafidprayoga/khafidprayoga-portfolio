0435d69725ed7de3320f5f0be4c7fd0d70dbace1
Khafid Prayoga <khafidp@pm.me>
2022-07-18 16:26:18 +0700

    add dotenv, and bio from env

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require("express")
-const app = express()
+require("dotenv").config()
+const { AGE, LOCATION, EMAIL } = process.env
 
+
+const app = express()
+app.disable("x-powered-by")
 app.get("/", (req, res) => {
-    res.send("Bye there!")
+    res.redirect("https://github.com/khafidprayoga")
+})
+
+app.get("/profile", (req, res) => {
+    const response = {
+        code: 200,
+        success: true,
+        message: "Hello World",
+        data: {
+            jobdesk: "Backend Developer",
+            age: AGE,
+            location: LOCATION,
+            email: EMAIL,
+        },
+        stacks: [
+            "Node.js",
+            "GO",
+            "AWS",
+            "Docker",
+        ],
+    }
+    res.status(response.code).send(response)
 })
 
 app.listen(3000, () => {
